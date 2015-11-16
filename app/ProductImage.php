1ff8cc0d0a4cd5dbccb49a2b3f48d336ac19767b
nhan <nhanarsenal0906@gmail.com>
2015-11-16 22:35:40 +0700

    update

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model {
 
 	protected $table= 'product_images';
-	//nhanh master
+	//nhanh rrelease
 
 	protected $fillable = ['image', 'product_id'];
 
